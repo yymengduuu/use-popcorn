b1dd2041577e29b4d7e206b2d8cf8529cdf31d14
yymengduuu <yymengduuu@gmail.com>
2025-06-01 02:55:00 +0100

    Auto-update: 2025-06-01 02:55:00

--- src/App.js
+++ src/App.js
@@ -343,7 +343,7 @@ function WatchedMovie({ movie, onDeleteWatched }) {
           <span>⏳</span>
           <span>{movie.runtime} min</span>
         </p>
-        <button className="btn-delete" onClick={onDeleteWatched}>
+        <button className="btn-delete" onClick={() => onDeleteWatched}>
           X
         </button>
       </div>
