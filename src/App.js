2b3c650d8721d6b6c55b82748cc3b528d14f72b1
yymengduuu <yymengduuu@gmail.com>
2025-06-01 02:49:49 +0100

    Auto-update: 2025-06-01 02:49:49

--- src/App.js
+++ src/App.js
@@ -311,11 +311,15 @@ function WatchedSummary({ watched }) {
   );
 }
 
-function WatchedMoviesList({ watched }) {
+function WatchedMoviesList({ watched, handleDeleteWatched }) {
   return (
     <ul className="list">
       {watched.map((movie) => (
-        <WatchedMovie movie={movie} key={movie.imdbID} />
+        <WatchedMovie
+          movie={movie}
+          key={movie.imdbID}
+          handleDeleteWatched={handleDeleteWatched}
+        />
       ))}
     </ul>
   );
